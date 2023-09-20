83454f2727e7f9814008ed525af5383039861c6f
Tual <tual.gokmen41@gmail.com>
2023-09-20 13:34:03 +0300

    Tual

---
+++ app/src/main/java/com/example/sqlliteuygulama/ConnectionHelper.java
@@ -0,0 +1,72 @@
+package com.example.sqlliteuygulama;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.StrictMode;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionHelper extends SQLiteOpenHelper {
+    Connection connection;
+    String username, password, ip, port, database;
+
+
+    public ConnectionHelper(Context context) {
+        super(context, "AndroidProje.db", null, 2);
+        ip = "192.168.2.208";
+        database = "AndroidProje";
+        username = "biltekbilisim";
+        password = "Bilisim20037816";
+        port = "1433";
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        String connectionUrl = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionUrl = "jdbc:jtds:sqlserver://" + ip + ":" + port + ";" + "databasename=" + database + ";user=" + username + ";password=" + password + ";";
+            connection = DriverManager.getConnection(connectionUrl);
+        } catch (Exception e) {
+            Toast.makeText(context, "HATA BAĞLANMILAMADI" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+
+
+    public void execSQL(String sorgu, Context co) throws SQLException {
+
+
+        try {
+
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(sorgu);
+
+
+        } catch (SQLException ex) {
+            throw new SQLException(ex);
+        }
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL("CREATE TABLE kullanicilar (ID INTEGER PRIMARY KEY AUTOINCREMENT, KullaniciAdi TEXT, Sifre TEXT, Email TEXT);");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS kullanicilar");
+        onCreate(sqLiteDatabase);
+    }
+}
